5cb2c3e7a76ec72362d18619602cd3c55d3e5495
Jenny Wong <jennyw981@gmail.com>
2019-05-01 16:56:24 -0400

    youtube video renders

--- client/app.js
+++ client/app.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import VideoPlayer from './components/videoPlayer'
+import VideoSearchBar from './components/videoSearchBar'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <VideoSearchBar />
     </div>
   )
 }
--- client/components/videoPlayer.js
+++ client/components/videoPlayer.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
-// Youtube Data Api
-const KEY ='AIzaSyCjL5n1rCIKqzIQTjt4I6at87Y5Y91JunU';
+const VideoPlayer = (props) => {
 
-// Youtube Data Api axios call
-const youtube = axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: KEY
-    }
-})
+    const videoSrc = `https://www.youtube.com/embed/${props.videoId}?autoplay=1`
 
-class VideoPlayer extends Component {
-
-    render () {
-
-        return (
-            <div className='video player'>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <iframe title='video player' style={{pointerEvents: 'none'}} src={videoSrc} />
+        </div>
+    )
 
 }
+
+export default VideoPlayer
