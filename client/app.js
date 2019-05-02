a054f991b59c6526a0f19526a6766b8c27ef755a
btan <btan7645@gmail.com>
2019-05-02 00:07:19 -0400

    resecured API key

--- client/app.js
+++ client/app.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-import {Navbar} from './components'
-import Routes from './routes'
-import VideoPlayer from './components/videoPlayer'
-import VideoSearchBar from './components/videoSearchBar'
+import { Navbar } from './components';
+import Routes from './routes';
+import VideoPlayer from './components/videoPlayer';
+import VideoSearchBar from './components/videoSearchBar';
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-      <VideoSearchBar />
-    </div>
-  )
-}
+	return (
+		<div>
+			<Navbar />
+			<Routes />
+			<VideoSearchBar />
+		</div>
+	);
+};
 
-export default App
+export default App;
--- client/components/videoPlayer.js
+++ client/components/videoPlayer.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 
 const VideoPlayer = (props) => {
+	const videoSrc = `https://www.youtube.com/embed/${props.videoId}?autoplay=1`;
+	return (
+		<div>
+			<iframe title="video player" style={{ pointerEvents: 'none' }} src={videoSrc} />
+		</div>
+	);
+};
 
-    const videoSrc = `https://www.youtube.com/embed/${props.videoId}?autoplay=1`
-
-    return (
-        <div>
-            <iframe title='video player' style={{pointerEvents: 'none'}} src={videoSrc} />
-        </div>
-    )
-
-}
-
-export default VideoPlayer
+export default VideoPlayer;
