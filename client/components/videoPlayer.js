7c1fcdde68153d792238e30af13805bf6f923f58
btan <btan7645@gmail.com>
2019-05-02 11:51:26 -0400

    added react-youtube

--- client/components/videoPlayer.js
+++ client/components/videoPlayer.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+import axios from 'axios'
+import YouTube from 'react-youtube'
 
-const VideoPlayer = (props) => {
-	const videoSrc = `https://www.youtube.com/embed/${props.videoId}?autoplay=1`;
-	return (
-		<div>
-			<iframe title="video player" style={{ pointerEvents: 'none' }} src={videoSrc} />
-		</div>
-	);
-};
+class VideoPlayer extends Component {
+  constructor() {
+    super()
+    // let videoSrc = `https://www.youtube.com/embed/${
+    //   this.props.videoId
+    // }?autoplay=0`
+  }
 
-export default VideoPlayer;
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    }
+    return (
+      <div>
+        {/* <iframe
+        title="video player"
+        style={{pointerEvents: 'none'}}
+        src={videoSrc}
+      /> */}
+        <YouTube videoId="2g811Eo7K8U" opts={opts} />
+      </div>
+    )
+  }
+}
+
+export default VideoPlayer
--- client/components/videoSearchBar.js
+++ client/components/videoSearchBar.js
@@ -1,60 +1,60 @@
-import React, { Component } from 'react';
-import VideoPlayer from './videoPlayer';
-import axios from 'axios';
+import React, {Component} from 'react'
+import VideoPlayer from './videoPlayer'
+import axios from 'axios'
 
 class VideoSearchBar extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			searchWords: '',
-			videoId: ''
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleClick = this.handleClick.bind(this);
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchWords: '',
+      videoId: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-	// Changes this.state.searchWords when user inputs a search word
-	handleChange(e) {
-		this.setState({
-			searchWords: e.target.value
-		});
-	}
+  // Changes this.state.searchWords when user inputs a search word
+  handleChange(e) {
+    this.setState({
+      searchWords: e.target.value
+    })
+  }
 
-	// Changes the state when user clicks submit
-	// Passes this.state.searchWords into Youtube Api to retrieve the top five videos
-	// Obtains the videoId and resets this.state.videoId
-	async handleClick() {
-		console.log('checking', this.state.searchWords);
-		const KEY = await axios.get('/api/youtubeapi');
-		const youtube = await axios.create({
-			baseURL: 'https://www.googleapis.com/youtube/v3',
-			params: {
-				part: 'snippet',
-				maxResults: 5,
-				key: KEY.data
-			}
-		});
-		const { data } = await youtube.get('/search', {
-			params: {
-				q: this.state.searchWords + `karaoke`
-			}
-		});
-		this.setState({
-			videoId: data.items[0].id.videoId
-		});
-	}
+  // Changes the state when user clicks submit
+  // Passes this.state.searchWords into Youtube Api to retrieve the top five videos
+  // Obtains the videoId and resets this.state.videoId
+  async handleClick() {
+    console.log('checking', this.state.searchWords)
+    const KEY = await axios.get('/api/youtubeapi')
+    const youtube = await axios.create({
+      baseURL: 'https://www.googleapis.com/youtube/v3',
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY.data
+      }
+    })
+    const {data} = await youtube.get('/search', {
+      params: {
+        q: this.state.searchWords + `karaoke`
+      }
+    })
+    this.setState({
+      videoId: data.items[0].id.videoId
+    })
+  }
 
-	render() {
-		return (
-			<div className="video searchbar">
-				<input placeholder="Start search here" onChange={this.handleChange} />
-				<button type="button" onClick={() => this.handleClick()}>
-					Submit
-				</button>
-				<VideoPlayer videoId={this.state.videoId} />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="video-searchbar">
+        <input placeholder="Start search here" onChange={this.handleChange} />
+        <button type="button" onClick={() => this.handleClick()}>
+          Submit
+        </button>
+        <VideoPlayer videoId={this.state.videoId} />
+      </div>
+    )
+  }
 }
 
-export default VideoSearchBar;
+export default VideoSearchBar
