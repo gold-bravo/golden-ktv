9f70fdc935a11239dc8f837d5012d89317576a8a
btan <btan7645@gmail.com>
2019-05-07 16:10:41 -0400

    lol

--- client/components/room.js
+++ client/components/room.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import VideoSearchBar from './videoSearchBar'
+import {OTSession, OTPublisher, OTStreams, OTSubscriber} from 'opentok-react'
 
 class Room extends Component {
   constructor(props) {
@@ -9,7 +10,21 @@ class Room extends Component {
 
   render() {
     console.log(this.props)
-    return <VideoSearchBar room={this.props.room.room} />
+    return (
+      <div>
+        <VideoSearchBar room={this.props.room.room} />
+        <OTSession
+          apiKey="46321722"
+          sessionId="2_MX40NjMyMTcyMn5-MTU1NzAyMjc5OTIwM35nSDNOVlMwUjlsUGFNbmZmTnZIeEhPeG1-fg"
+          token="T1==cGFydG5lcl9pZD00NjMyMTcyMiZzaWc9MzliMzE4ZmMwZjViODdhZjE0YmI0MmFiNDRhOTQxMmFkYTJkMmQ0ODpzZXNzaW9uX2lkPTJfTVg0ME5qTXlNVGN5TW41LU1UVTFOekF5TWpjNU9USXdNMzVuU0ROT1ZsTXdVamxzVUdGTmJtWm1UblpJZUVoUGVHMS1mZyZjcmVhdGVfdGltZT0xNTU3MDIyNzk5JnJvbGU9bW9kZXJhdG9yJm5vbmNlPTE1NTcwMjI3OTkuMjIwMTEyMjc3NzIzOTY="
+        >
+          <OTPublisher />
+          <OTStreams>
+            <OTSubscriber />
+          </OTStreams>
+        </OTSession>
+      </div>
+    )
   }
 }
 
