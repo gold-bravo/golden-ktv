62e71e62a5776acd112cb721cde6cae1357a5889
btan <btan7645@gmail.com>
2019-05-08 10:55:51 -0400

    working

--- client/components/room.js
+++ client/components/room.js
@@ -4,19 +4,16 @@ import VideoSearchBar from './videoSearchBar'
 import {OTSession, OTPublisher, OTStreams, OTSubscriber} from 'opentok-react'
 
 class Room extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     console.log(this.props)
     return (
       <div>
         <VideoSearchBar room={this.props.room.room} />
         <OTSession
-          apiKey="46321722"
-          sessionId="2_MX40NjMyMTcyMn5-MTU1NzAyMjc5OTIwM35nSDNOVlMwUjlsUGFNbmZmTnZIeEhPeG1-fg"
-          token="T1==cGFydG5lcl9pZD00NjMyMTcyMiZzaWc9MzliMzE4ZmMwZjViODdhZjE0YmI0MmFiNDRhOTQxMmFkYTJkMmQ0ODpzZXNzaW9uX2lkPTJfTVg0ME5qTXlNVGN5TW41LU1UVTFOekF5TWpjNU9USXdNMzVuU0ROT1ZsTXdVamxzVUdGTmJtWm1UblpJZUVoUGVHMS1mZyZjcmVhdGVfdGltZT0xNTU3MDIyNzk5JnJvbGU9bW9kZXJhdG9yJm5vbmNlPTE1NTcwMjI3OTkuMjIwMTEyMjc3NzIzOTY="
+          //TODO: Please fill out keys
+          apiKey=""
+          sessionId=""
+          token=""
         >
           <OTPublisher />
           <OTStreams>
