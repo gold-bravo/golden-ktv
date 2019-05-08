5de474b0a80ffc3cfbb60ef1b148fb5632047d4d
Jenny Wong <jennyw981@gmail.com>
2019-05-08 16:49:38 -0400

    made videoResults component and queue

--- client/components/room.js
+++ client/components/room.js
@@ -9,17 +9,17 @@ class Room extends Component {
     return (
       <div>
         <VideoSearchBar room={this.props.room.room} />
-        <OTSession
+        {/* <OTSession
           //TODO: Please fill out keys
-          apiKey=""
-          sessionId=""
-          token=""
+          apiKey="46322342"
+          sessionId="1_MX40NjMyMjM0Mn5-MTU1NzM0MTA0NDYxNn5RT3Z5cUtUN1gwWXM3cDZyQkdnNnNKWHJ-fg"
+          token="T1==cGFydG5lcl9pZD00NjMyMjM0MiZzaWc9OTM2YWQxN2ZhZjlmZTBlMDg1ZDBjMDNiNjJlZTM3NTI0ZDE1MTg1NjpzZXNzaW9uX2lkPTFfTVg0ME5qTXlNak0wTW41LU1UVTFOek0wTVRBME5EWXhObjVSVDNaNWNVdFVOMWd3V1hNM2NEWnlRa2RuTm5OS1dISi1mZyZjcmVhdGVfdGltZT0xNTU3MzQxMDY0Jm5vbmNlPTAuNjA4NjY4MTQ2OTc3NTMwNCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTU5OTMzMDY0JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9"
         >
           <OTPublisher />
           <OTStreams>
             <OTSubscriber />
           </OTStreams>
-        </OTSession>
+        </OTSession> */}
       </div>
     )
   }
