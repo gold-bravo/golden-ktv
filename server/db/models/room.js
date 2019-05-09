d319a3263e17e6a448fae350f2ebfbf5bff15b45
Chongyoon Nah <chongyoonaaronnah@gmail.com>
2019-05-09 12:02:35 -0400

    adding roomnum to room model

--- server/db/models/room.js
+++ server/db/models/room.js
@@ -3,6 +3,7 @@ const db = require('../db')
 
 const Room = db.define('Rooms', {
   curTime: {type: Sequelize.FLOAT},
+  roomNum: {type: Sequelize.STRING},
   queue: {type: Sequelize.ARRAY(Sequelize.STRING)},
   users: {type: Sequelize.ARRAY(Sequelize.STRING)},
   sessionId: {type: Sequelize.STRING}
