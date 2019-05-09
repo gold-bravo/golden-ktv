448ffe47ee1b551d9933a214ccfed126e8f794ca
btan <btan7645@gmail.com>
2019-05-09 11:15:34 -0400

    deleted Dakota test

--- test.js
+++
@@ -1,6 +0,0 @@
-const OpenTok = require('opentok')
-let key = "3634172", secret='0eba37f885cd66387b1e26688b03c0e1f4f94304'
-let opentok = new OpenTok(key, secret)
-opentok.createSession((err, session) => {
-  console.log("asdf", "err", err)
-})
