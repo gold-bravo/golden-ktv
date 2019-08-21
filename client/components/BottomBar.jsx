736746f65e7d42a377236ea1a4ef971fb055f1fa
btan <btan7645@gmail.com>
2019-08-21 12:05:50 -0400

    fixed bottom-bar

--- client/components/BottomBar.jsx
+++ client/components/BottomBar.jsx
@@ -4,7 +4,7 @@ const BottomBar = () => {
   return (
     <div id="bottom-bar">
       <a href="https://github.com/cieltan/golden-ktv">
-        CHECK OUT OUR SOURCE CODE!
+        CHECK OUT OUR SOURCE CODE:
         <img src="/images/github.png" />
       </a>
     </div>
